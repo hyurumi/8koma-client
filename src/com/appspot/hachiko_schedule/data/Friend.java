077d2b6a125501efd10d4234a9b97069d4adfc5d
daisy1754 <d-part@hotmail.co.jp>
2013-05-18 13:23:30 +0900

    make Friend Percelable so we can pass them on intent

--- src/com/appspot/hachiko_schedule/data/Friend.java
+++ src/com/appspot/hachiko_schedule/data/Friend.java
@@ -1,11 +1,13 @@
 package com.appspot.hachiko_schedule.data;
 
+import android.os.Parcel;
+import android.os.Parcelable;
 import com.google.common.base.Preconditions;
 
 /**
  * Data class for friend
  */
-public class Friend {
+public class Friend implements Parcelable {
     String name;
     String phoneNo;
     String email;
@@ -42,4 +44,33 @@ public class Friend {
     public String getEmail() {
         return email;
     }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeString(phoneNo);
+        dest.writeString(email);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<Friend> CREATOR = new Creator<Friend>() {
+
+        @Override
+        public Friend createFromParcel(Parcel source) {
+            return new Friend(
+                    source.readString(),
+                    source.readString(),
+                    source.readString()
+            );
+        }
+
+        @Override
+        public Friend[] newArray(int size) {
+            return new Friend[size];
+        }
+    };
 }
