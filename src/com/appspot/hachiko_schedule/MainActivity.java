40cfe1367cb9e2809c03b7a3b3806d14d9495523
daisy1754 <d-part@hotmail.co.jp>
2013-08-03 19:55:22 +0900

    bugfix: reuse fragment if possible in MainActivity; no duplicate fragments :)

--- src/com/appspot/hachiko_schedule/MainActivity.java
+++ src/com/appspot/hachiko_schedule/MainActivity.java
@@ -1,9 +1,6 @@
 package com.appspot.hachiko_schedule;
 
-import android.app.ActionBar;
-import android.app.Activity;
-import android.app.Fragment;
-import android.app.FragmentTransaction;
+import android.app.*;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.*;
@@ -27,11 +24,11 @@ public class MainActivity extends Activity {
         actionBar.addTab(actionBar.newTab()
                 .setText(getString(R.string.settled_schedules))
                 .setTabListener(new TabListener<SettledEventsFragment>(
-                        this, "settled_events", SettledEventsFragment.class)));
+                        "settled_events", SettledEventsFragment.class)));
         actionBar.addTab(actionBar.newTab()
                 .setText(getString(R.string.unsettled_schedules))
                 .setTabListener(new TabListener<UnsettledEventsFragment>(
-                        this, "unsettled_events", UnsettledEventsFragment.class)));
+                        "unsettled_events", UnsettledEventsFragment.class)));
         checkNewEvent(getIntent());
     }
 
@@ -81,40 +78,33 @@ public class MainActivity extends Activity {
         startActivity(intent);
     }
 
-    static private class TabListener<T extends Fragment> implements ActionBar.TabListener {
+    private class TabListener<T extends Fragment> implements ActionBar.TabListener {
         private Fragment mFragment;
-        private final Activity mActivity;
         private final String mTag;
         private final Class<T> mClass;
 
         /** Constructor used each time a new tab is created.
-         * @param activity  The host Activity, used to instantiate the fragment
+         *
          * @param tag  The identifier tag for the fragment
          * @param clz  The fragment's Class, used to instantiate the fragment
          */
-        public TabListener(Activity activity, String tag, Class<T> clz) {
-            mActivity = activity;
+        public TabListener(String tag, Class<T> clz) {
             mTag = tag;
             mClass = clz;
         }
 
-    /* The following are each of the ActionBar.TabListener callbacks */
-
         public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
-            // Check if the fragment is already initialized
+            mFragment = getFragmentManager().findFragmentByTag(mTag);
             if (mFragment == null) {
-                // If not, instantiate and add it to the activity
-                mFragment = Fragment.instantiate(mActivity, mClass.getName());
+                mFragment = Fragment.instantiate(MainActivity.this, mClass.getName());
                 ft.add(android.R.id.content, mFragment, mTag);
             } else {
-                // If it exists, simply attach it in order to show it
                 ft.attach(mFragment);
             }
         }
 
         public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
             if (mFragment != null) {
-                // Detach the fragment, because another one is being attached
                 ft.detach(mFragment);
             }
         }
