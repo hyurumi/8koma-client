deae483417075a9dda4a5aff2c97bfd084c3e9fa
daisy1754 <d-part@hotmail.co.jp>
2013-08-04 11:29:49 +0900

    introduced an IS_DEVELOPER constant

--- src/com/appspot/hachiko_schedule/Constants.java
+++ src/com/appspot/hachiko_schedule/Constants.java
@@ -4,6 +4,11 @@ package com.appspot.hachiko_schedule;
  * Constants used among classes
  */
 public class Constants {
+    /**
+     * 開発中はtrueにされるべきフラグ，これで内部設定の見せる・見せないを制御
+     */
+    public static final boolean IS_DEVELOPER = true;
+
     public static final String EXTRA_KEY_FRIENDS = "extra_key_friends";
     public static final String EXTRA_KEY_EVENT_TYPE = "extra_key_event_type";
     public static final String EXTRA_KEY_EVENT_CANDIDATE_SCHEDULES
