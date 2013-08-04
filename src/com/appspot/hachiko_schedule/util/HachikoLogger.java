631bec89f735f629c8aa4744d3c96a9dd3f1b1f8
daisy1754 <d-part@hotmail.co.jp>
2013-08-04 11:32:55 +0900

    add IS_DEV check for logger and add one utility method

--- src/com/appspot/hachiko_schedule/util/HachikoLogger.java
+++ src/com/appspot/hachiko_schedule/util/HachikoLogger.java
@@ -2,6 +2,8 @@ package com.appspot.hachiko_schedule.util;
 
 import android.util.Log;
 
+import static com.appspot.hachiko_schedule.Constants.IS_DEVELOPER;
+
 /**
  * Wrapper class of {@link Log}
  */
@@ -9,13 +11,37 @@ public class HachikoLogger {
     private static final String DEFAULT_TAG = "HachikoApp";
 
     static public int debug(String msg) {
+        if (!IS_DEVELOPER) {
+            return 0;
+        }
         return Log.d(DEFAULT_TAG, msg);
     }
 
     static public int debug(Object... objects) {
+        if (!IS_DEVELOPER) {
+            return 0;
+        }
         StringBuilder builder = new StringBuilder();
         for (Object obj: objects) {
             builder.append(obj).append(' ');
+        }
+        return Log.d(DEFAULT_TAG, builder.toString());
+    }
+
+    static public int debugWithSeparator(char ch, Object... objects) {
+        if (!IS_DEVELOPER) {
+            return 0;
+        }
+        return debugWithSeparator(Character.toString(ch), objects);
+    }
+
+    static public int debugWithSeparator(CharSequence separator, Object... objects) {
+        if (!IS_DEVELOPER) {
+            return 0;
+        }
+        StringBuilder builder = new StringBuilder();
+        for (Object obj: objects) {
+            builder.append(obj).append(' ').append(separator);
         }
         return Log.d(DEFAULT_TAG, builder.toString());
     }
