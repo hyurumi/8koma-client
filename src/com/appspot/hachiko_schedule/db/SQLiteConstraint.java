a737de8ba34c201f5abd0e704f6c8b0456f428b2
daisy1754 <d-part@hotmail.co.jp>
2013-09-26 22:56:00 +0900

    define constants for sqlite

---
+++ src/com/appspot/hachiko_schedule/db/SQLiteConstraint.java
@@ -0,0 +1,10 @@
+package com.appspot.hachiko_schedule.db;
+
+/**
+ * SQLiteの制約を表す文字列定数たち.
+ */
+public class SQLiteConstraint {
+    public final static String AUTOINCREMENT = "autoincrement";
+    public final static String NOT_NULL = "not null";
+    public final static String PRIMARY_KEY = "primary key";
+}
---
+++ src/com/appspot/hachiko_schedule/db/SQLiteType.java
@@ -0,0 +1,11 @@
+package com.appspot.hachiko_schedule.db;
+
+/**
+ * SQLiteのデータ型に対応する文字列定数たち
+ */
+public class SQLiteType {
+    public static final String BLOB = "blob";
+    public static final String TEXT = "text";
+    public static final String INTEGER = "integer";
+    public static final String REAL = "real";
+}
