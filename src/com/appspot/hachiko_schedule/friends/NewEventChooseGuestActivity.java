1beb937d3d7328408f2c8497c37cb667fdeac5cc
daisy1754 <d-part@hotmail.co.jp>
2013-09-28 11:43:19 +0900

    add menu on choose guest view to show event_list and config

--- src/com/appspot/hachiko_schedule/friends/NewEventChooseGuestActivity.java
+++ src/com/appspot/hachiko_schedule/friends/NewEventChooseGuestActivity.java
@@ -1,13 +1,19 @@
 package com.appspot.hachiko_schedule.friends;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Filter;
 import android.widget.Filterable;
 import android.widget.ListView;
 import android.widget.SearchView;
+import com.appspot.hachiko_schedule.EventListActivity;
 import com.appspot.hachiko_schedule.R;
+import com.appspot.hachiko_schedule.prefs.MainPreferenceActivity;
 import com.appspot.hachiko_schedule.util.HachikoLogger;
 
 /**
@@ -29,6 +35,37 @@ public class NewEventChooseGuestActivity extends Activity {
         // 安全でないキャスト… ListView#setFilterTextを直に叩くと出てくるポップアップを表示したくないため，
         // Filterを直に触っている．たぶんListViewのサブクラスをよしなにつくるほうがベター
         filter = ((Filterable) friendListView.getAdapter()).getFilter();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.choose_friend_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_see_events:
+                launchEventListActivity();
+                return true;
+            case R.id.action_config:
+                launchMenuActivity();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void launchEventListActivity() {
+        Intent intent = new Intent(this, EventListActivity.class);
+        startActivity(intent);
+    }
+
+    private void launchMenuActivity() {
+        Intent intent = new Intent(this, MainPreferenceActivity.class);
+        startActivity(intent);
     }
 
     private class SearchTextListener implements SearchView.OnQueryTextListener {
