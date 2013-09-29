c4c70cdff87358f9f790c47d90a0042066d9c4fe
daisy1754 <d-part@hotmail.co.jp>
2013-09-29 23:42:01 +0900

    replace event list by new UnfixedPlanView

--- src/com/appspot/hachiko_schedule/plans/EventListActivity.java
+++ src/com/appspot/hachiko_schedule/plans/EventListActivity.java
@@ -1,31 +1,30 @@
 package com.appspot.hachiko_schedule.plans;
 
-import android.app.ActionBar;
 import android.app.Activity;
-import android.app.Fragment;
-import android.app.FragmentTransaction;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.CalendarContract;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+import android.view.*;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 import com.appspot.hachiko_schedule.Constants;
 import com.appspot.hachiko_schedule.R;
+import com.appspot.hachiko_schedule.data.CandidateDate;
+import com.appspot.hachiko_schedule.data.UnfixedPlan;
 import com.appspot.hachiko_schedule.friends.NewEventChooseGuestActivity;
 import com.appspot.hachiko_schedule.prefs.MainPreferenceActivity;
 import com.appspot.hachiko_schedule.push.GoogleCloudMessagingHelper;
 import com.appspot.hachiko_schedule.setup.SetupManager;
+import com.google.common.collect.ImmutableList;
 
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.Date;
 
 public class EventListActivity extends Activity {
-
-    private static final String KEY_SELECTED_TAB = "selected_tab";
-    private GoogleCloudMessagingHelper googleCloudMessagingHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +35,51 @@ public class EventListActivity extends Activity {
             finish();
             return;
         }
-        googleCloudMessagingHelper = new GoogleCloudMessagingHelper(this);
+
+        initGoogleCloudMessagingIfNecessary();
+
+        setContentView(R.layout.activity_event_list);
+        ((ListView) findViewById(R.id.event_list)).setAdapter(new PlanAdapter(this,
+                new UnfixedPlan[] {
+                        new UnfixedPlan(
+                                "Hachikoミーティング @ 7号館",
+                                ImmutableList.of("古見元気", "臼山直人", "竹井悠人"),
+                                ImmutableList.of(
+                                        new CandidateDate("10/10 10:00 - 12:00", ImmutableList.of("古見元気", "臼山直人"), ImmutableList.of("竹井悠人")),
+                                        new CandidateDate("10/11 12:00 - 14:00", ImmutableList.of("古見元気"), ImmutableList.of("臼山直人", "竹井悠人")),
+                                        new CandidateDate("10/12 10:00 - 12:00", ImmutableList.of("古見元気"), ImmutableList.of("竹井悠人")))
+                                ),
+                        new UnfixedPlan(
+                                "Hachiko開発",
+                                ImmutableList.of("古見元気", "臼山直人", "竹井悠人", "古見元気", "臼山直人", "竹井悠人", "古見元気", "臼山直人", "竹井悠人"),
+                                ImmutableList.of(
+                                        new CandidateDate("10/10 10:00 - 12:00", ImmutableList.of("古見元気", "臼山直人"), ImmutableList.of("竹井悠人")),
+                                        new CandidateDate("10/11 12:00 - 14:00", ImmutableList.of("古見元気"), ImmutableList.of("臼山直人", "竹井悠人")),
+                                        new CandidateDate("10/12 10:00 - 12:00", ImmutableList.of("古見元気"), Collections.<String>emptyList()),
+                                        new CandidateDate("10/10 10:00 - 12:00", ImmutableList.of("古見元気", "臼山直人"), ImmutableList.of("竹井悠人")),
+                                        new CandidateDate("10/11 12:00 - 14:00", ImmutableList.of("古見元気"), ImmutableList.of("臼山直人", "竹井悠人")))
+                        ),
+                        new UnfixedPlan(
+                                "Hachikoミーティング @ 7号館",
+                                ImmutableList.of("古見元気", "臼山直人", "竹井悠人"),
+                                ImmutableList.of(
+                                        new CandidateDate("10/10 10:00 - 12:00", ImmutableList.of("古見元気", "臼山直人"), ImmutableList.of("竹井悠人")),
+                                        new CandidateDate("10/11 12:00 - 14:00", ImmutableList.of("古見元気"), ImmutableList.of("臼山直人", "竹井悠人")),
+                                        new CandidateDate("10/12 10:00 - 12:00", ImmutableList.of("古見元気"), Collections.<String>emptyList()),
+                                        new CandidateDate("10/10 10:00 - 12:00", ImmutableList.of("古見元気", "臼山直人"), ImmutableList.of("竹井悠人")),
+                                        new CandidateDate("10/11 12:00 - 14:00", ImmutableList.of("古見元気"), ImmutableList.of("臼山直人", "竹井悠人")))
+                        )
+                }));
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        checkNewEvent(intent);
+    }
+
+    private void initGoogleCloudMessagingIfNecessary() {
+        GoogleCloudMessagingHelper googleCloudMessagingHelper = new GoogleCloudMessagingHelper(this);
         if (!googleCloudMessagingHelper.checkPlayServices()) {
             return;
         }
@@ -44,41 +87,6 @@ public class EventListActivity extends Activity {
         if (registrationId.isEmpty()) {
             googleCloudMessagingHelper.registerInBackground();
         }
-
-        final ActionBar actionBar = getActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-        actionBar.addTab(actionBar.newTab()
-                .setText(getString(R.string.settled_schedules))
-                .setTabListener(new TabListener<SettledEventsFragment>(
-                        "settled_events", SettledEventsFragment.class)));
-        actionBar.addTab(actionBar.newTab()
-                .setText(getString(R.string.unsettled_schedules))
-                .setTabListener(new TabListener<UnsettledEventsFragment>(
-                        "unsettled_events", UnsettledEventsFragment.class)));
-        checkNewEvent(getIntent());
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        outState.putInt(KEY_SELECTED_TAB,getActionBar().getSelectedNavigationIndex());
-        super.onSaveInstanceState(outState);
-    }
-
-    @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-        if (savedInstanceState != null) {
-            int lastSelectedTabIndex = savedInstanceState.getInt(KEY_SELECTED_TAB, -1);
-            if (lastSelectedTabIndex >= 0) {
-                getActionBar().selectTab(getActionBar().getTabAt(lastSelectedTabIndex));
-            }
-        }
-    }
-
-    @Override
-    protected void onNewIntent(Intent intent) {
-        super.onNewIntent(intent);
-        checkNewEvent(intent);
     }
 
     private void checkNewEvent(Intent intent) {
@@ -133,39 +141,26 @@ public class EventListActivity extends Activity {
         startActivity(intent);
     }
 
-    private class TabListener<T extends Fragment> implements ActionBar.TabListener {
-        private Fragment mFragment;
-        private final String mTag;
-        private final Class<T> mClass;
+    /**
+     * 確定した予定一覧を表示する用のリストadapter
+     */
+    private static class PlanAdapter extends ArrayAdapter<UnfixedPlan> {
+        private final UnfixedPlan[] plans;
 
-        /** Constructor used each time a new tab is created.
-         *
-         * @param tag  The identifier tag for the fragment
-         * @param clz  The fragment's Class, used to instantiate the fragment
-         */
-        public TabListener(String tag, Class<T> clz) {
-            mTag = tag;
-            mClass = clz;
-        }
-
-        public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
-            mFragment = getFragmentManager().findFragmentByTag(mTag);
-            if (mFragment == null) {
-                mFragment = Fragment.instantiate(EventListActivity.this, mClass.getName());
-                ft.add(android.R.id.content, mFragment, mTag);
-            } else {
-                ft.attach(mFragment);
-            }
-        }
-
-        public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
-            if (mFragment != null) {
-                ft.detach(mFragment);
-            }
+        public PlanAdapter(
+                Context context, UnfixedPlan[] unfixedPlans) {
+            super(context, R.layout.unfixed_plan_view, unfixedPlans);
+            this.plans = unfixedPlans;
         }
 
         @Override
-        public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = new UnfixedPlanView(getContext());
+            }
+            ((UnfixedPlanView) convertView).setPlan(plans[position]);
+
+            return convertView;
         }
     }
 }
