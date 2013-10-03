66ddebc930f6fca768002f478788f6819cb279d3
daisy1754 <d-part@hotmail.co.jp>
2013-10-04 07:32:46 +0900

    Userテーブルを簡素化

--- src/com/appspot/hachiko_schedule/db/UserTableHelper.java
+++ src/com/appspot/hachiko_schedule/db/UserTableHelper.java
@@ -10,35 +10,25 @@ import android.database.sqlite.SQLiteDatabase;
  */
 public class UserTableHelper {
     private static final String USER_TABLE_NAME = "users";
-    private static final String PHONE_TABLE_NAME = "phones";
-    private static final String EMAIL_TABLE_NAME = "emails";
     private static final String HACHIKO_ID = "hachiko_id";
+    private static final String IS_HACHIKO_USER = "is_haciko_user";
     private static final String DISPLAY_NAME = "display_name";
-    private static final String FB_ID = "fb_id";
     private static final String LOCAL_CONTACT_ID = "local_contact_id";
-    private static final String PROFILE_PIC = "profile_pic";
-    private static final String PHONE = "phone_no";
-    private static final String EMAIL = "address";
+    private static final String PROFILE_PIC_URI = "profile_pic_uri";
+    private static final String PRIMARY_EMAIL = "primary_email";
 
     private HachikoDBOpenHelper dbHelper;
 
     public static void onCreateDatabase(SQLiteDatabase database) {
         String createUsersTable = new SQLiteCreateTableBuilder(USER_TABLE_NAME)
                 .addColumn(HACHIKO_ID, SQLiteType.TEXT, SQLiteConstraint.PRIMARY_KEY)
+                .addColumn(IS_HACHIKO_USER, SQLiteType.INTEGER)
                 .addColumn(DISPLAY_NAME, SQLiteType.TEXT, SQLiteConstraint.NOT_NULL)
-                .addColumn(PROFILE_PIC, SQLiteType.BLOB)
-                .addColumn(FB_ID, SQLiteType.INTEGER)
+                .addColumn(PROFILE_PIC_URI, SQLiteType.TEXT)
                 .addColumn(LOCAL_CONTACT_ID, SQLiteType.INTEGER)
+                .addColumn(PRIMARY_EMAIL, SQLiteType.TEXT)
                 .toString();
-        String createPhonesTable = new SQLiteCreateTableBuilder(PHONE_TABLE_NAME)
-                .addColumn(HACHIKO_ID, SQLiteType.TEXT, SQLiteConstraint.PRIMARY_KEY)
-                .addColumn(PHONE, SQLiteType.TEXT)
-                .toString();
-        String createEmailTable = new SQLiteCreateTableBuilder(EMAIL_TABLE_NAME)
-                .addColumn(HACHIKO_ID, SQLiteType.TEXT, SQLiteConstraint.PRIMARY_KEY)
-                .addColumn(EMAIL, SQLiteType.TEXT)
-                .toString();
-        database.execSQL(createUsersTable + createPhonesTable + createEmailTable);
+        database.execSQL(createUsersTable);
     }
 
     public static void onUpgradeDatabase(SQLiteDatabase db, int oldVersion, int newVersion) {}
@@ -47,13 +37,15 @@ public class UserTableHelper {
         dbHelper = new HachikoDBOpenHelper(context, null);
     }
 
-    public void insertUser(
-            String hachikoId, String displayName, String fbId, String localContact,
-            String phone, String email) {
+    public void insertUser(String hachikoId, String displayName, String localContact,
+                           String profilePicUri, String primaryEmail) {
         ContentValues values = new ContentValues();
         values.put(HACHIKO_ID, hachikoId);
+        values.put(IS_HACHIKO_USER, 0);
         values.put(DISPLAY_NAME, displayName);
-        values.put(FB_ID, fbId);
+        values.put(PROFILE_PIC_URI, profilePicUri);
+        values.put(LOCAL_CONTACT_ID, localContact);
+        values.put(PRIMARY_EMAIL, primaryEmail);
         SQLiteDatabase db = dbHelper.getWritableDatabase();
         db.insert(USER_TABLE_NAME, null, values);
         db.close();
