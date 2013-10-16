5ea4f1bd0dcbe71e17392480f81444e531a113ac
daisy1754 <d-part@hotmail.co.jp>
2013-10-16 16:06:52 +0900

    Dateのフォーマットあれこれあって困る問題，アドホックに対応

--- src/com/appspot/hachiko_schedule/util/DateUtils.java
+++ src/com/appspot/hachiko_schedule/util/DateUtils.java
@@ -44,7 +44,12 @@ public class DateUtils {
             }
             return new SimpleDateFormat(DATE_FORMAT_ISO8601).parse(iso8601);
         } catch (ParseException e) {
-            HachikoLogger.error("Date parse error" + iso8601, e);
+            try {
+                HachikoLogger.error("Error parsing " + iso8601 + ", assume as UTC", e);
+                return new SimpleDateFormat(DATE_FORMAT_ISO8601_WITHOUT_TIMEZONE).parse(iso8601.substring(0, 19));
+            } catch (ParseException e1) {
+                HachikoLogger.error("Date parse error" + iso8601.substring(0, 19), e);
+            }
             return null;
         }
     }
