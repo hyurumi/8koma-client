c29e6368d7d1dd20bf9d71bd01df9e6296c243dd
daisy1754 <d-part@hotmail.co.jp>
2013-10-19 00:00:40 +0900

    IS_DEVELOPERをtrueに戻した DeployGateの仕様を勘違いしていた．最新版が自動でダウンロード版になるわけでないので，当面はリポジトリ上ではこのフラグは常にtrueで，リリース時にfalseにしたものをビルドしたい

--- src/com/appspot/hachiko_schedule/Constants.java
+++ src/com/appspot/hachiko_schedule/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
     /**
      * 開発中はtrueにされるべきフラグ，これで内部設定の見せる・見せないを制御
      */
-    public static final boolean IS_DEVELOPER = false;
+    public static final boolean IS_DEVELOPER = true;
 
     public static final String EXTRA_KEY_FRIENDS = "extra_key_friends";
     public static final String EXTRA_KEY_NEW_EVENT = "extra_key_new_event";
