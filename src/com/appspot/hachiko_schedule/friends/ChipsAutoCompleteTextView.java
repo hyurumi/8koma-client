0a743d482f0376f7386b39ecfc49b6d37a4e7605
daisy1754 <d-part@hotmail.co.jp>
2013-10-20 14:55:29 +0900

    友達一覧で選んだ名前chipにアイコンを追加

--- src/com/appspot/hachiko_schedule/friends/ChipsAutoCompleteTextView.java
+++ src/com/appspot/hachiko_schedule/friends/ChipsAutoCompleteTextView.java
@@ -4,17 +4,20 @@ package com.appspot.hachiko_schedule.friends;
 import android.content.Context;
 import android.text.Spannable;
 import android.text.SpannableStringBuilder;
-import android.text.style.BackgroundColorSpan;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.MultiAutoCompleteTextView;
 import com.appspot.hachiko_schedule.R;
+import com.appspot.hachiko_schedule.ui.TexttipSpan;
 import com.appspot.hachiko_schedule.util.HachikoLogger;
 import com.google.common.base.Joiner;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 // https://github.com/kpbird/chips-edittext-libraryを参考に実装
 public class ChipsAutoCompleteTextView
@@ -74,8 +77,9 @@ public class ChipsAutoCompleteTextView
         int x =0;
         for(String chipText : chips){
             spannableStringBuilder.setSpan(
-                    new BackgroundColorSpan(
-                            getContext().getResources().getColor(R.color.friend_name_gray)),
+                    new TexttipSpan(getContext(), R.layout.friend_name_tip,
+                            R.id.friend_name_tip_name, R.id.friend_name_tip_image,
+                            R.drawable.ic_contact_picture, chipText),
                     x, x + chipText.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
             x = x + chipText.length() + 2;
         }
---
+++ src/com/appspot/hachiko_schedule/ui/TexttipSpan.java
@@ -0,0 +1,45 @@
+package com.appspot.hachiko_schedule.ui;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.text.style.ImageSpan;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * アイコンとテキストをワンセットにした{@link android.text.Spannable}のサブクラス．
+ * https://github.com/kpbird/chips-edittext-libraryを参考に実装
+ */
+public class TexttipSpan extends ImageSpan {
+    public TexttipSpan(Context context, int viewResource, int textViewId,
+                       int imageViewId, int iconResource, CharSequence text) {
+        super(context,
+                createBitmap(context, viewResource, textViewId, imageViewId, iconResource, text));
+    }
+
+    private static Bitmap createBitmap(Context context, int viewResource, int textViewId,
+                                       int imageViewId, int iconResource, CharSequence text) {
+        LayoutInflater inflater
+                = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(viewResource, null);
+        ((TextView) view.findViewById(textViewId)).setText(text);
+        ((ImageView) view.findViewById(imageViewId)).setImageDrawable(
+                context.getResources().getDrawable(iconResource));
+        int spec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
+        view.measure(spec, spec);
+        view.layout(0, 0, view.getMeasuredWidth(), view.getMeasuredHeight());
+        Bitmap b = Bitmap.createBitmap(
+                view.getWidth(), view.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(b);
+        canvas.translate(-view.getScrollX(), -view.getScrollY());
+        view.draw(canvas);
+        view.setDrawingCacheEnabled(true);
+        Bitmap viewBmp = view.getDrawingCache().copy(Bitmap.Config.ARGB_8888, true);
+        view.destroyDrawingCache();
+        return viewBmp;
+    }
+}
